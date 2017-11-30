b1fe6bedd060923aa69f26bc6370db09669f9012
Sebastian Arjona <arjona@Sebastians-iMac.local>
2017-11-29 20:59:17 -0600

    Initial Commit

---
+++ Ventas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ventas
+//
+//  Created by Sebastian Arjona on 11/29/17.
+//  Copyright © 2017 Sebastian Arjona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
